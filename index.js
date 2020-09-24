e9f6665fc85b5bcecf78e07e0326b022ff88db44
dzakyinsan20 <igabakardzaky@gmail.com>
2020-09-24 22:39:07 +0700

    ''

---
+++ index.js
@@ -0,0 +1,95 @@
+let indexImage = 0;
+let score = 0;
+const image = [
+  {
+    imageFull: "sasuke.jpg",
+    imageCrop: "sasuke-crop.jpg",
+    answear: "sasuke",
+    choice1: "sasuke",
+    choice2: "itachi",
+    choice3: "andika",
+    choice4: "shisui",
+  },
+  {
+    imageFull: "ino.jpg",
+    imageCrop: "ino-crop.jpg",
+    answear: "ino",
+    choice1: "sunade",
+    choice2: "deidara",
+    choice3: "ino",
+    choice4: "sakura",
+  },
+  {
+    imageFull: "sarutobi.jpg",
+    imageCrop: "sarutobi-crop.jpg",
+    answear: "Hiruzen Sarutobi",
+    choice1: "Izuma Sarutobi",
+    choice2: "Hiruzen Sarutobi",
+    choice3: "Konohamaru",
+    choice4: "Mirai Sarutobi",
+  },
+  {
+    imageFull: "jiraiya.png",
+    imageCrop: "jiraiya-crop.png",
+    answear: "Jiraiya",
+    choice1: "Kiba",
+    choice2: "Izuma ",
+    choice3: "Sarutobi",
+    choice4: "Jiraiya",
+  },
+  {
+    imageFull: "patrick.jpg",
+    imageCrop: "patrick-crop.jpg",
+    answear: "patrick akatsuki",
+    choice1: "konan akatsuki",
+    choice2: "konan akatsuki",
+    choice3: "kisame akatsuki",
+    choice4: "patrick akatsuki",
+  },
+];
+
+function gererateImage() {
+  document.getElementById("stage").innerText = indexImage + 1;
+  const imageElement = document.getElementById("srcImg");
+  for (let k = 0; k < 4; k++) {
+    document.getElementsByName(`answearBtn`)[k].innerText = image[indexImage][`choice${k + 1}`];
+    document.getElementsByName(`answearBtn`)[k].value = image[indexImage][`choice${k + 1}`];
+  }
+  imageElement.src = image[indexImage].imageCrop;
+  return image[indexImage].answear;
+}
+
+let showImage = gererateImage();
+
+for (let i = 0; i < 4; i++) {
+  const cekValueButton = document.getElementsByName(`answearBtn`)[i];
+  cekValueButton.addEventListener("click", function () {
+    if (image[indexImage].answear === cekValueButton.value) {
+      alert("Yosh!! BETULLL");
+      score++;
+      document.getElementById("score").innerText = score;
+      const imageElement = document.getElementById("srcImg");
+      imageElement.src = image[indexImage].imageFull;
+      for (let j = 1; j <= 4; j++) {
+        const disabledBtn = document.getElementById(`buttonId${j}`);
+        disabledBtn.disabled = true;
+      }
+    } else {
+      alert("Nani!! salahh...");
+      for (let j = 1; j <= 4; j++) {
+        const disabledBtn = document.getElementById(`buttonId${j}`);
+        disabledBtn.disabled = true;
+      }
+    }
+    
+  });
+}
+const nextButton = document.getElementById("next");
+nextButton.addEventListener("click", function () {
+  indexImage++;
+  for (let j = 1; j <= 4; j++) {
+    const disabledBtn = document.getElementById(`buttonId${j}`);
+    disabledBtn.disabled = false;
+  }
+  gererateImage();
+});
